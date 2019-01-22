c151d22fd828bfb270600a236f34bd4dc5c7215d
Jordan Brennan <jordanbrennan@me.com>
2019-01-22 14:17:40 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const hbs = exphbs.create({extname:'.hbs', defaultLayout:'main.hbs', layoutsDir: `${__dirname}/views/layouts`});
 
 // Express set up
 const app = express();
@@ -10,19 +11,12 @@ app.use(function(req, res, next) {
   next();
 });
 
-const hbs = exphbs.create({
-    extname:'.hbs',
-    defaultLayout:'main.hbs',
-    layoutsDir: `${__dirname}/views/layouts`
-});
 app.use(express.static('public'));
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 
-
 // Routes
 app.get('/', (req, res) => res.render('home'));
-
 
 // Start server
 app.listen(3000, () => console.log('App running at http://localhost:3000'));
