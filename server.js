e5b26c55132c7db238d7713788cad3fe910fa011
Jordan Brennan <jordanbrennan@me.com>
2019-01-22 14:28:30 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
-const hbs = exphbs.create({extname:'.hbs', defaultLayout:'main.hbs', layoutsDir: `${__dirname}/views/layouts`});
+const hbs = exphbs.create({extname:'.hbs', defaultLayout:'main.hbs'});
 
 // Express set up
 const app = express();
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", ['GET']);
-  next();
-});
-
 app.use(express.static('public'));
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
